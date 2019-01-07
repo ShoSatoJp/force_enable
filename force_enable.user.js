d4371de685d1f03ca4e86b6b1dc5f4322b346942
佐藤翔 <shosatojp2001@outlook.jp>
2019-01-07 16:21:42 +0900

    -

--- force_enable.user.js
+++ force_enable.user.js
@@ -12,11 +12,60 @@
 
 (function () {
     'use strict';
+    const TYPE_LIST = ['copy', 'cut', 'paste', 'selectstart', 'keydown', 'contextmenu'];
+    //before loading
+    //event handlers
+    const Element_addEventListener_ = Element.prototype.addEventListener;
+    const Window_addEventListener_ = window.addEventListener;
 
-    const addEventListener_ = Element.prototype.addEventListener;
+    function check(type, fn) {
+        if (~TYPE_LIST.indexOf(type)) {
+            console.log(type);
+        } else {
+            fn && fn();
+        }
+    }
+
     Element.prototype.addEventListener = function (type, listener, ev) {
-        console.log(type);
-        addEventListener_(type, listener, ev);
+        check(type, (() => Element_addEventListener_.bind(this, type, listener, ev)()).bind(this));
     };
-    
+    window.addEventListener = function (type, listener, ev) {
+        check(type, (() => Window_addEventListener_.bind(window, type, listener, ev)()).bind(this));
+    }
+
+    //after loading
+    Window_addEventListener_('load', function () {
+        //attributes
+        const MAX_DEPTH = 1000;
+        const START = Date.now();
+        const RESULT_ELEMENT = document.createElement('p');
+        let DEPTH = 0;
+        let COUNT = 0;
+        let PROCESSED = 0;
+        let TIME = 0;
+        document.body.appendChild(RESULT_ELEMENT);
+
+        function remove_attribute(e, fn) {
+            TYPE_LIST.forEach(x => {
+                const name = 'on' + x;
+                if (e.hasAttribute(name)) {
+                    e.removeAttribute(name);
+                    fn && fn();
+                }
+                if (e[name]) {
+                    e[name] = null;
+                }
+            });
+        }
+        (function f(e, d = 0) {
+            remove_attribute(e, () => PROCESSED++);
+            d++ >= DEPTH && (DEPTH = d);
+            if (d >= MAX_DEPTH) return;
+            Array.from(e.children).forEach(x => f(x, d));
+            COUNT++;
+        })(document.body);
+
+        TIME = Date.now() - START;
+        console.log(TIME + 'ms');
+    });
 })();
