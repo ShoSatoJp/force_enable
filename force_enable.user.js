07f7a8d90c7d84b74389f1fc52445f8252a0522a
佐藤翔 <shosatojp2001@outlook.jp>
2019-01-08 11:14:20 +0900

    -

--- force_enable.user.js
+++ force_enable.user.js
@@ -2,103 +2,146 @@
 // @name         Force Enable
 // @namespace    https://github.com/ShoSatoJp
 // @version      0.1
-// @description  force enable text select, copy, paste, right click and more...
+// @description  force enable text selection, copy, paste and right click and disable smooth scroll.
 // @author       ShoSato
 // @match        *://*/*
 // @grant        none
+// @run-at document-start
 // @updateURL https://raw.githubusercontent.com/ShoSatoJp/force_enable/master/force_enable.user.js
 // @downloadURL https://raw.githubusercontent.com/ShoSatoJp/force_enable/master/force_enable.user.js
 // ==/UserScript==
 
-(function () {
-    'use strict';
+(function force_enable(W, use_load_event = true) {
+    if (W['shosato_force_enable']) {
+        return;
+    } else {
+        W['shosato_force_enable'] = true;
+    }
+    const D = W.document;
+    const E = W.Element;
+
+    const Element_addEventListener_ = E.prototype.addEventListener;
+    const Window_addEventListener_ = W.addEventListener;
+    const Document_addEventListener_ = D.addEventListener;
+
     const TYPE_LIST = ['copy', 'cut', 'paste', 'selectstart', 'mousedown', 'keydown', 'contextmenu', 'scroll', 'mousewheel'];
     const TYPE_LIST_WITH_ON = TYPE_LIST.map(x => 'on' + x);
-    // const RESULT_ELEMENT = document.createElement('p');
+    const RESULT_ELEMENT = D.createElement('p');
     let PROCESSED = 0;
     let COUNT = 0;
-    // document.body.appendChild(RESULT_ELEMENT);
+    let TIME = 0;
+    D.body && D.body.appendChild(RESULT_ELEMENT);
 
-    //before loading
-    //event handlers
-    const Element_addEventListener_ = Element.prototype.addEventListener;
-    const Window_addEventListener_ = window.addEventListener;
-    const Document_addEventListener_ = document.addEventListener;
-
-    function check(type, fn) {
-        if (~TYPE_LIST.indexOf(type)) {} else {
-            fn && fn();
-        }
+    function show_result() {
+        RESULT_ELEMENT.textContent = '';
     }
 
-    Element.prototype.addEventListener = function (type, listener, ev) {
-        check(type, (() => Element_addEventListener_.bind(this, type, listener, ev)()).bind(this));
-    };
-    window.addEventListener = function (type, listener, ev) {
-        check(type, (() => Window_addEventListener_.bind(window, type, listener, ev)()).bind(this));
-    };
-    document.addEventListener = function (type, listener, ev) {
-        check(type, (() => Document_addEventListener_.bind(window, type, listener, ev)()).bind(this));
-    };
+    //event handlers
+    {
+        function check(type, fn) {
+            if (!~TYPE_LIST.indexOf(type)) fn && fn();
+        }
 
-    //after loading
-    Window_addEventListener_('load', function () {
+        E.prototype.addEventListener = function (type, listener, option) {
+            check(type, (() => Element_addEventListener_.bind(this, type, listener, option)()).bind(this));
+        };
+        W.addEventListener = function (type, listener, option) {
+            check(type, (() => Window_addEventListener_.bind(W, type, listener, option)()).bind(this));
+        };
+        D.addEventListener = function (type, listener, option) {
+            check(type, (() => Document_addEventListener_.bind(W, type, listener, option)()).bind(this));
+        };
+    }
+
+    function after_loading() {
         const START = Date.now();
-        let TIME = 0;
 
         //attributes & properties
-        const MAX_DEPTH = 1000;
-        let DEPTH = 0;
+        {
+            const MAX_DEPTH = 1000;
+            let DEPTH = 0;
 
-        function remove_attribute(e, fn) {
-            TYPE_LIST_WITH_ON.forEach(name => {
-                if (e.hasAttribute && e.hasAttribute(name)) {
-                    e.removeAttribute(name);
-                    fn && fn();
-                }
-                if (e[name]) {
-                    e[name] = null;
-                    fn && fn();
-                }
-            });
+            function remove_attribute(e, fn) {
+                TYPE_LIST_WITH_ON.forEach(name => {
+                    if (e.hasAttribute && e.hasAttribute(name)) {
+                        e.removeAttribute(name);
+                        fn && fn();
+                    }
+                    if (e[name]) {
+                        e[name] = null;
+                        fn && fn();
+                    }
+                });
+            }
+
+            function remove_attribute_recurcive(target) {
+                (function f(e, d = 0) {
+                    remove_attribute(e, () => PROCESSED++);
+                    d++ >= DEPTH && (DEPTH = d);
+                    if (d >= MAX_DEPTH) return;
+                    Array.from(e.children).forEach(x => f(x, d));
+                    COUNT++;
+                })(target);
+            }
+            W.remove_attribute_recurcive = remove_attribute_recurcive
+            remove_attribute_recurcive(D);
         }
-        (function f(e, d = 0) {
-            remove_attribute(e, () => PROCESSED++);
-            d++ >= DEPTH && (DEPTH = d);
-            if (d >= MAX_DEPTH) return;
-            Array.from(e.children).forEach(x => f(x, d));
-            COUNT++;
-        })(document);
 
         //transparent background color. disabled selection by css.
-        const style = document.createElement('style');
-        style.textContent = `::selection {
-            background-color: black !important;
-            color: white !important;
+        {
+            const style = D.createElement('style');
+            style.textContent = `::selection {
+                background-color: black !important;
+                color: white !important;
+            }
+            * , body {
+                -webkit-touch-callout: auto !important;
+                -webkit-user-select: auto !important;
+                -khtml-user-select: auto !important;
+                    -moz-user-select: auto !important;
+                    -ms-user-select: auto !important;
+                        user-select: auto !important;
+                scroll-behavior: auto !important;
+            }`;
+            D.body && D.body.appendChild(style);
         }
-        * {
-            -webkit-touch-callout: auto !important;
-              -webkit-user-select: auto !important;
-               -khtml-user-select: auto !important;
-                 -moz-user-select: auto !important;
-                  -ms-user-select: auto !important;
-                      user-select: auto !important;
-            scroll-behavior: auto !important;
-        }`;
-        document.body && document.body.appendChild(style);
+
         // dynamic change
-        (new MutationObserver(function (records) {
-            console.log(records);
-            records.forEach(x => {
+        {
+            D.body && (new MutationObserver(function (records) {
+                records.forEach(x => {
+                    if (x.type === 'childList' && x.addedNodes.length) {
+                        x.addedNodes.forEach(n => {
+                            if (n instanceof E) remove_attribute_recurcive(n);
+                        });
+                    } else if (x.type === 'attributes' && ~['style', 'class'].indexOf(x.attributeName)) {
+                        remove_attribute_recurcive(x.target);
+                    }
+                });
+            })).observe(D.body, {
+                attributes: true,
+                childList: true,
+                characterData: true,
+                attributeFilter: ['style', 'class'],
+                subtree: true,
             });
-        })).observe(document.body, {
-            attributes: true,
-            childList: true,
-            characterData: true,
-            attributeFilter: ['style', 'class'],
-            subtree: true,
-        });
+        }
+
+        //iframe
+        {
+            D.querySelectorAll('iframe').forEach(x => {
+                try {
+                    force_enable(x.contentWindow, false);
+                } catch {}
+            });
+        }
+
         TIME = Date.now() - START;
         console.log(TIME + 'ms');
-    });
-})();
+    }
+    if (use_load_event) {
+        Window_addEventListener_('load', after_loading);
+    } else {
+        after_loading();
+    }
+})(window, true);
